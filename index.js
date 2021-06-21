3a26a79efe8dd9590b2fc4f1de72dbaf2e7d4f53
Kaiser Samrat <kaisersamrat136@gmail.com>
2021-06-21 22:08:18 +0600

    user details  api added

--- index.js
+++ index.js
@@ -16,49 +16,72 @@ client.connect(err => {
     const jobCollection = client.db("job-seeking").collection("jobList");
     const appliedJobCollection = client.db("job-seeking").collection("AppliedJob");
 
+    app.get('/', (req, res) => {
+        res.send('Hello World!')
+    })
     app.get('/jobList', (req, res) => {
-        jobCollection.find({})
+        const search = req.query.search
+        jobCollection.find({ title: { $regex: search } })
             .toArray((err, documents) => {
 
                 res.send(documents)
             })
     })
-    app.get('/job/:id',(req,res)=>{
+    app.get('/job/:id', (req, res) => {
         const id = req.params.id;
-        jobCollection.find({_id: ObjectID(id)}).toArray((err,documents)=>{
-          res.send(documents[0])
+        jobCollection.find({ _id: ObjectID(id) }).toArray((err, documents) => {
+            res.send(documents[0])
         })
-      })
-      app.post('/applyJob',(req,res)=>{
+    })
+    app.get('/allJob', (req, res) => {
+        jobCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+    app.get('/myJobList', (req, res) => {
+        appliedJobCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+
+    app.get('/myJobList/:email', (req, res) => {
+        const email = req.params.email;
+        appliedJobCollection.find({email: email})
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
+    })
+    app.post('/applyJob', (req, res) => {
         const appliedJobs = req.body;
-        appliedJobCollection.insertOne(appliedJobs,(err,result)=>{
-            console.log(err,result);
-            res.send({count: result.insertedCount})
+        appliedJobCollection.insertOne(appliedJobs, (err, result) => {
+            console.log(err, result);
+            res.send({ count: result.insertedCount })
 
         })
     })
-    app.post('/addJob',(req,res)=>{
+    app.post('/addJob', (req, res) => {
         const Jobs = req.body;
-        jobCollection.insertOne(Jobs,(err,result)=>{
-            console.log(err,result);
-            res.send({count: result.insertedCount})
+        jobCollection.insertOne(Jobs, (err, result) => {
+            console.log(err, result);
+            res.send({ count: result.insertedCount })
 
         })
     })
-   
 
-    // app.post('/addJob',(req,res)=>{
-    //     const jobs = req.body;
-    //     jobCollection.insertMany(jobs,(err,result)=>{
-    //         console.log(err,result);
-    //         res.send({count: result})
+    app.delete('/deleteJob/:id', (req, res) => {
+        const id = req.params.id;
+        jobCollection.deleteOne({ _id: ObjectID(id) }, (err) => {
+            if (!err) {
+                res.send({ count: 1 })
+            }
+        })
 
-    //     })
-    // })
-   
-    app.get('/', (req, res) => {
-        res.send('Hello World!')
     })
+
+
+
 
 });
 
